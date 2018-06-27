da9444e15b3f8d1cdf50149535a9e968295415ec
佐々木健佑 <sasakikensuke@MBP-N5UME4JR.local>
2018-06-27 20:21:24 +0900

    add send query function

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,16 @@ FROM python:3.6
 
 RUN groupadd -r uwsgi && useradd -r -g uwsgi uwsgi
 
-RUN pip install uWSGI==2.0.8 \
+RUN pip install pulp \
+                uWSGI \
                 dash==0.21.1 \
                 dash-renderer==0.12.1 \
                 dash-html-components==0.10.1 \
-                dash-core-components==0.22.1  \
-                plotly \
-                pandas \
-                pulp \
-                time \
-                string
+                dash-core-components==0.13.0-rc5 \
+                loremipsum \
+                #dash-core-components==0.22.1  \
+                dash_table_experiments \
+                pandas
 
 WORKDIR /app
 COPY app /app
--- app/app.py
+++ app/app.py
@@ -2,12 +2,10 @@ import os
 import io
 import sys
 import dash
-import MeCab
-import base64
-import neologdn
+import sqlite3
+import loremipsum
 import numpy as np
 import pandas as pd
-import plotly.graph_objs as go
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table_experiments as dt
@@ -16,18 +14,26 @@ from dash.dependencies import Input, Output, State
 
 from flask import send_from_directory
 
+# import dash
+# from dash.dependencies import Input, Output
+# import dash_core_components as dcc
+# import dash_html_components as html
+from loremipsum import get_sentences
+
+
 
 app = dash.Dash()
-app.title = 'Training OPT'
+app.title = 'Query Share'
 # app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
 app.config.suppress_callback_exceptions = True
 
+
 # --------------------------- preprocessing ---------------------------
 
 # load objects or other (To use it, like "processing.objname")
-import processing
-
+# conn = sqlite3.connect("iris_db.db")
+# df_iris = pd.read_sql("select * from iris_test", conn)
 
 # --------------------------- define function -------------------------
 
@@ -55,8 +61,8 @@ title_components = html.Div([
     html.Div([
         html.Div([
             html.Div([
-                html.Div(["Related Articles"], id="header"),
-                html.Div([dcc.Link('選手データを追加する', href='/add_logs_to_models', id='header-right')],
+                html.Div(["Query Share"], id="header"),
+                html.Div([dcc.Link('Write Query', href='/add_player', id='header-right')],
                          style={'cursor': 'pointer', 'float': 'right', 'margin-right': '7%'}),
                 html.Div([dcc.Link('TOP', href='/', id='header-right')],
                          style={'cursor': 'pointer', 'float': 'right'})
@@ -66,7 +72,7 @@ title_components = html.Div([
 
     # title
     html.Div([
-        html.H1(['TRAINING OPT'], id='title_h')
+        html.H1(['Query Share'], id='title_h')
     ], style={'width': '100%', 'text-align':'center'})
 ])
 
@@ -87,87 +93,134 @@ page_1_layout = html.Div([
 
     title_components,
 
+    # html.Div([
+    #     html.Div(
+    #         html.H1("練習メニューを最適化します！")
+    #         )
+    # ], style={'width': '100%', 'text-align':'center'}),
+
     # dropdown div
     html.Div([
 
-        # define dropdown input
-        html.Div([
-            dcc.Dropdown(
-                placeholder='please select article title ...',
-                id='dropdown-input',
-                options=[{'label': i, 'value': j} for i,j in zip(processing.body_data.title.unique(),
-                                                                 processing.body_data.article_num.unique())]
-            )
-        ], style={'margin-bottom': '3%',
-                  'width': '90%',
-                  'font-size': '12px',
-                  'font-style': 'Noto Sans JP',
-                  'position': 'auto', 'margin': 'auto'}),
-
-        # define the way of recommendation
-        html.Div([
-            dcc.RadioItems(
-            # dcc.Checklist(
-                options=[{'label': 'Nginx log', 'value': 'way_log'},
-                         {'label': 'Contents', 'value': 'way_contents'},
-                         {'label': 'Titles', 'value': 'way_titles'}],
-                id='radio-input',
-                className='state',
-                labelClassName='state',
-                value='way_log',
-                labelStyle={'display': 'inline-block',
-                            'margin': '3% 6%',
-                            'color': '#373939',
-                            'font-size': '24px',
-                            'font-style': 'bold',
-                            'font-family' : 'Montserrat'})
-        ], className='pretty', style={'position': 'auto', 'margin': 'auto', 'text-align': 'center'}),
-
-
-        # define table app_layout
-        html.Div([
-            dt.DataTable(
-                rows=[{}], # initialise the rows
-                header_row_height="50px",
-                enable_drag_and_drop=False,
-                # min_width="100%",
-                row_selectable=False,
-                filterable=False,
-                sortable=True,
-                # selected_row_indices=[],
-                id='datatable'
-            )], style={'width': '90%', 'position': 'auto', 'margin': 'auto', 'max-width':'1100px'}
+    html.Div([
+        dcc.Tabs(
+            tabs=[
+                {'label': 'get Treatment data', 'value': 1},
+                {'label': 'get Diseasename data', 'value': 2},
+                {'label': 'merge all data', 'value': 3},
+                {'label': 'test query', 'value': 4},
+            ],
+            value=1,
+            id='tabs',
+            vertical=False
         ),
+        html.Div(id='tab-output')
+    ], style={
+        'width': '100%',
+        'margin-left': 'auto',
+        'margin-right': 'auto',
+        'margin-bottom': '5%'
+    }),
+
+
 
         footer_components
 
     ],
     style={'width': '100%', 'position': 'auto', 'margin': 'auto'}),
+
 ],
 id='wrapper',
 style={'position': 'relative', 'width': '100%', 'font-family': 'Noto Sans JP'})
 
-# Update tables in a callback
-@app.callback(Output('datatable', 'rows'),
-              [Input('dropdown-input', 'value'),
-               Input('radio-input', 'value')])
-def update_datatable(user_selection, radio_value):
-    """
-    For user selections, return the relevant table
-    """
-    if radio_value=="way_log":
-        top_df_use = processing.log_data
-    elif radio_value=="way_contents":
-        top_df_use = processing.body_data
-    else:
-        top_df_use = processing.title_data
 
-    returned_df = processing.show_selected_table(df=top_df_use,
-                                                 df_link=processing.link_data,
-                                                 df_rellink=processing.rellink_data,
-                                                 input_value=user_selection,
-                                                 input_rvalue=radio_value)
-    return returned_df
+
+@app.callback(Output('tab-output', 'children'), [Input('tabs', 'value')])
+def display_content(value):
+    test_query = [
+        "SELECT * FROM linnerud_test where jumps > 100;",
+        "SELECT * \nFROM iris\nWHERE Species = 'setosa'",
+        "SELECT * FROM iris WHERE ...",
+        "select x from test order by x collate reverse",
+    ]
+
+    return html.Div([
+        html.Br(),
+        html.Div([
+        dcc.Textarea(
+            id="input_query",
+            placeholder='Please write query',
+            value=test_query[value-1],
+            style={'width': '99%',
+                   'display': 'table',
+                   'position': 'auto',
+                   'margin': 'auto',
+                   'margin-left':'2px',
+                   'height': "200px",
+                   'font-family':'Monaco',
+                   'font-size': "18px",
+                   'border': 'solid 2px #5DC8BB',
+                   'border-radius': '4px'
+                   }
+
+        )],style={'width' : '100%'}
+        ),
+    html.Button('RUN', id='button',
+                className='square_btn',
+                style={'width': '20%',
+                'display': 'table',
+                'position': 'auto',
+                'margin': 'auto'}
+                ),
+    # define table app_layout
+    html.Div([
+        dt.DataTable(
+            rows=[{}], # initialise the rows
+            # header_row_height="50px",
+            enable_drag_and_drop=False,
+            # min_width="100%",
+            row_selectable=False,
+            filterable=False,
+            sortable=True,
+            id='datatable'
+        )], style={'width': '90%',
+                   'height': '90%',
+                   'position': 'auto',
+                   'margin': 'auto',
+                   'max-width':'1100px',
+                   'margin-top': '24px',
+                   'margin-bottom':'24px'}
+    )
+    ], style={
+        'background-color': "white"
+    })
+@app.callback(Output('datatable', 'rows'),
+              [Input('button', 'n_clicks')],
+              [State('input_query', 'value')])
+def execte_query(n_clicks, value):
+    conn = sqlite3.connect("data/test_db.db")
+    # define statement
+    statement = value
+    df_fromdb = pd.read_sql(statement, conn)
+    return df_fromdb.to_dict('records')
+    # table name is linnerud_test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -191,20 +244,14 @@ page_2_layout = html.Div([
 
     html.Div([
         html.Div([
-                html.H2("新規記事タイトルを入力してください.."),
+                html.H2("Please write your query name."),
                 dcc.Input(id='input-box',
                           type='url',
                           placeholder='please write Article title ...',
-                          style=input_style),
-                html.H2("新規記事本文を入力してください.."),
-                dcc.Textarea(
-                    id='input-contents',
-                    placeholder='please write Article contents ...',
-                    style=input_style
-                )
+                          style=input_style)
                   ],
                  style={'width': '100%'}),
-        html.Button('Submit', id='button', className='square_btn', style={'margin-bottom': '3%'}),
+        html.Button('Create', id='button', className='square_btn', style={'margin-bottom': '3%'}),
         html.Div(id='output-container-button',
                  children='Enter a value and press submit')
     ], style={'width': '100%', 'color': '#373939', 'font-style': 'Noto Sans JP' }),
@@ -215,116 +262,14 @@ page_2_layout = html.Div([
 style={'position': 'relative', 'width': '100%', 'font-family': 'Noto Sans JP'})
 
 
-def lower_text(text):
-    return text.lower()
-
-def extractKeyword(text):
-    # normalize text
-    text = neologdn.normalize(lower_text(text))
-    # define tagger
-    tagger = MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
-    node = tagger.parseToNode(text)#textがu''形式⇒『.encode()』が必要
-    keywords = []
-    while node:
-        if node.feature.split(",")[0] == "名詞" or node.feature.split(",")[0] == "動詞":
-            keywords.append(node.surface)
-        node = node.next
-    return keywords
-
 @app.callback(Output('output-container-button', 'children'),
               [Input('button', 'n_clicks')],
               [State('input-box', 'value')])
 def update_output(n_clicks, value):
-    keywords = extractKeyword(value)
-    res = []
-    for w in keywords:
-        res.append(w)
     return 'RESULT : "{}" CLICKED {} times'.format(
-        res,
+        value,
         n_clicks
     )
-
-
-# define page3 layout -------------------------------------------------------------------------------
-
-
-page_3_layout = html.Div([
-
-    title_components,
-
-    html.Div([
-        html.Div([
-            dcc.Upload(
-                id='upload-data',
-                children=html.Div([
-                    'Drag and Drop or ',
-                    html.A('Select Files')
-                ]),
-                style={
-                    'width': '100%',
-                    'height': '60px',
-                    'lineHeight': '60px',
-                    'borderWidth': '1px',
-                    'borderStyle': 'dashed',
-                    'borderRadius': '5px',
-                    'textAlign': 'center',
-                    'margin': '10px'
-                },
-                multiple=False)
-        ])
-    ], style={'width': '100%','position': 'auto', 'margin': 'auto'}),
-
-    html.Div(dt.DataTable(rows=[{}], id='table')),
-
-
-    footer_components
-
-],id='wrapper',
-style={'position': 'relative', 'width': '100%', 'font-family': 'Montserrat'})
-
-
-# file upload function
-def parse_contents(contents, filename):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv( io.StringIO(decoded.decode('utf-8')) )
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-
-    except Exception as e:
-        print(e)
-        return None
-
-    return df
-
-
-
-
-# callback table creation
-@app.callback(Output('table', 'rows'),
-              [Input('upload-data', 'contents'),
-               Input('upload-data', 'filename')])
-def update_output(contents, filename):
-    if contents is not None:
-        # このparse_contentsで今はcsvを読み込んでdfに代入する仕様になってて、
-        # このparse_contentsの他に新しく関数を定義して、jsonを読み込んで記事×記事データを返してくれるようにしてくれると良いです！！
-        df = parse_contents(contents, filename)
-        if df is not None:
-            return df.to_dict('records')
-        else:
-            return [{}]
-    else:
-        return [{}]
-
-
-
-# end of page3 layout -------------------------------------------------------------------------------
-
 
 # define whole layout ------------------------
 
@@ -344,10 +289,8 @@ app.layout = html.Div([
 def display_page(pathname):
     if pathname == '/':
         return page_1_layout
-    elif pathname == '/add_articles_to_models':
+    elif pathname == '/add_player':
         return page_2_layout
-    elif pathname == '/add_logs_to_models':
-        return page_3_layout
     # You could also return a 404 "URL not found" page here
 
 
